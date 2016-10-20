e1d25fd558ff23d087fa3416bd78695366b66a4a
KwakesProject <kwakes@me.com>
2016-10-20 16:23:27 +0100

    redux promise add

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -10,6 +10,7 @@
 /// <reference path="globals/react-redux/index.d.ts" />
 /// <reference path="globals/react-router/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
+/// <reference path="globals/redux-promise/index.d.ts" />
 /// <reference path="globals/redux/index.d.ts" />
 /// <reference path="globals/webpack-dev-server/index.d.ts" />
 /// <reference path="globals/webpack/index.d.ts" />
